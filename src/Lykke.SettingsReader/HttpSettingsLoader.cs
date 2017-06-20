19d294dbb605f3ba22abba98a05b9dbeb9d403ed
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-06-20 12:30:50 +0300

    HTTP settings loader added

---
+++ src/Lykke.SettingsReader/HttpSettingsLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Lykke.SettingsReader
+{
+    public static class HttpSettingsLoader
+    {
+        public static TSettings Load<TSettings>(string settingsUrl = null)
+        {
+            settingsUrl = settingsUrl ?? Environment.GetEnvironmentVariable("SettingsUrl");
+
+            if (string.IsNullOrEmpty(settingsUrl))
+            {
+                throw new Exception("settingsUrl not specified and environment variable 'SettingsUrl' is not defined");
+            }
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = httpClient.GetAsync(settingsUrl).Result)
+                {
+                    var settingsData = response.Content.ReadAsStringAsync().Result;
+
+                    return SettingsProcessor.Process<TSettings>(settingsData);
+                }
+            }
+        }
+    }
+}
