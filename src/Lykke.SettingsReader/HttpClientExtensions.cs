cd0120f2f0fb0beb21b22dfbe8c8ca5c6b605fed
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-06-20 12:58:33 +0300

    HttpClient extension LoadSettings was extracted

---
+++ src/Lykke.SettingsReader/HttpClientExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Lykke.SettingsReader
+{
+    public static class HttpClientExtensions
+    {
+        /// <summary>
+        /// Loads and parse settings from given <param name="settingsUrl"/> if any,
+        /// otherwise settings will be loaded from url specified by "SettingsUrl" environment variable
+        /// </summary>
+        /// <typeparam name="TSettings">Type of setting to load</typeparam>
+        /// <param name="httpClient">Http client</param>
+        /// <param name="settingsUrl">Settings url</param>
+        /// <returns>Loaded and parsed settings</returns>
+        public static TSettings LoadSettings<TSettings>(this HttpClient httpClient, string settingsUrl = null)
+        {
+            settingsUrl = settingsUrl ?? Environment.GetEnvironmentVariable("SettingsUrl");
+
+            if (string.IsNullOrEmpty(settingsUrl))
+            {
+                throw new Exception("settingsUrl not specified and environment variable 'SettingsUrl' is not defined");
+            }
+
+            using (var response = httpClient.GetAsync(settingsUrl).Result)
+            {
+                var settingsData = response.Content.ReadAsStringAsync().Result;
+
+                return SettingsProcessor.Process<TSettings>(settingsData);
+            }
+        }
+    }
+}
--- src/Lykke.SettingsReader/HttpSettingsLoader.cs
+++ src/Lykke.SettingsReader/HttpSettingsLoader.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
 
 namespace Lykke.SettingsReader
 {
     public static class HttpSettingsLoader
     {
+        /// <summary>
+        /// Loads and parse settings from given <param name="settingsUrl"/> if any,
+        /// otherwise settings will be loaded from url specified by "SettingsUrl" environment variable
+        /// </summary>
+        /// <typeparam name="TSettings">Type of setting to load</typeparam>
+        /// <param name="settingsUrl">Settings url</param>
+        /// <returns>Loaded and parsed settings</returns>
         public static TSettings Load<TSettings>(string settingsUrl = null)
         {
-            settingsUrl = settingsUrl ?? Environment.GetEnvironmentVariable("SettingsUrl");
-
-            if (string.IsNullOrEmpty(settingsUrl))
-            {
-                throw new Exception("settingsUrl not specified and environment variable 'SettingsUrl' is not defined");
-            }
-
             using (var httpClient = new HttpClient())
             {
-                using (var response = httpClient.GetAsync(settingsUrl).Result)
-                {
-                    var settingsData = response.Content.ReadAsStringAsync().Result;
-
-                    return SettingsProcessor.Process<TSettings>(settingsData);
-                }
+                return httpClient.LoadSettings<TSettings>();
             }
         }
     }
