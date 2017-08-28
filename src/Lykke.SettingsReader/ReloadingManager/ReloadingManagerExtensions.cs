3535772a91b53fe18259a6ba9ecec91de65e5d7b
Albert Ibragimov <a@prosto-a.ru>
2017-08-28 12:19:21 +0300

    Update Tests

--- src/Lykke.SettingsReader/ReloadingManager/ReloadingManagerExtensions.cs
+++ src/Lykke.SettingsReader/ReloadingManager/ReloadingManagerExtensions.cs
@@ -3,8 +3,9 @@ using System;
 namespace Lykke.SettingsReader
 {
     public static class ReloadingManagerExtensions {
-        public static IReloadingManager<TValue> For<TRoot, TValue>(this IReloadingManager<TRoot> rootManager, Func<TRoot, TValue> expr) {
-            return new NestedReloadingManager<TRoot, TValue>(rootManager, expr);
+        public static IReloadingManager<string> ConnectionString<TRoot>(this IReloadingManager<TRoot> rootManager, Func<TRoot, string> expr)
+        {
+            return new ConnectionStringReloadingManager<TRoot>(rootManager, expr);
         }
     }
 }
