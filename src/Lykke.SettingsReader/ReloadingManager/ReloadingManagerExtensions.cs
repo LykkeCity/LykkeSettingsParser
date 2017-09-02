0128c940e7d6d44bd01ac5a3a953460811afd476
Albert Ibragimov <a@prosto-a.ru>
2017-09-02 15:15:35 +0300

    add NestedReloadingManager

--- src/Lykke.SettingsReader/ReloadingManager/ReloadingManagerExtensions.cs
+++ src/Lykke.SettingsReader/ReloadingManager/ReloadingManagerExtensions.cs
@@ -3,9 +3,15 @@ using System;
 namespace Lykke.SettingsReader
 {
     public static class ReloadingManagerExtensions {
+
+        public static IReloadingManager<T> Nested<TRoot, T>(this IReloadingManager<TRoot> rootManager, Func<TRoot, T> expr)
+        {
+            return new NestedReloadingManager<TRoot, T>(rootManager, expr);
+        }
+
         public static IReloadingManager<string> ConnectionString<TRoot>(this IReloadingManager<TRoot> rootManager, Func<TRoot, string> expr)
         {
-            return new ConnectionStringReloadingManager<TRoot>(rootManager, expr);
+            return new NestedReloadingManager<TRoot, string>(rootManager, expr);
         }
     }
 }
