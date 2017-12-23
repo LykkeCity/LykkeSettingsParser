a61242d7cc086d8a9b289fd6da46508ab7b336a9
Dmitry Samodov <samodov.dmitry@gmail.com>
2017-12-24 01:25:45 +0300

    added ConstantReloadingManager

---
+++ src/Lykke.SettingsReader/ReloadingManager/ConstantReloadingManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Lykke.SettingsReader.ReloadingManager
+{
+    public class ConstantReloadingManager<T> : ReloadingManagerBase<T>
+    {
+        private readonly T _value;
+
+        public ConstantReloadingManager(T value)
+        {
+            _value = value;
+        }
+
+        protected override Task<T> Load()
+        {
+            return Task.FromResult(_value);
+        }
+    }
+}
