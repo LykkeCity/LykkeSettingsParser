983290ae6c3c8f4146e1fa884bacdd97386b6250
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-01-11 00:57:08 +0300

    removed flurl dependency

---
+++ src/Lykke.SettingsReader/SettingsProcessor/Checkers/HttpCheckerClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Lykke.SettingsReader.Checkers
+{
+    public sealed class HttpCheckerClient
+    {
+        static readonly HttpClient instance=new HttpClient();
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static HttpCheckerClient()
+        {
+        }
+
+        HttpCheckerClient()
+        {
+        }
+
+        public static HttpClient Instance => instance;
+    }
+}
