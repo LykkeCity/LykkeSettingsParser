4f4d0839306c12e14035e46403e7a39fae6f05ac
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-01-21 20:15:48 +0300

    fix for negative numbers convesion

---
+++ tests/Lykke.SettingsReader.Test/Models/TestConvert.cs
@@ -0,0 +1,10 @@
+﻿namespace Lykke.SettingsReader.Test.Models
+{
+    public class TestConvert
+    {
+        public int Int { get; set; }
+        public double Double { get; set; }
+        public decimal Decimal { get; set; }
+        public float Float { get; set; }
+    }
+}
