91817e7c1b4782f49d547901d13e850ef48a98b2
nikagamkrelidze <nikagamkrelidze233@gmail.com>
2018-06-29 20:00:41 +0400

    mongo checker added

---
+++ src/Lykke.SettingsReader/SettingsProcessor/Checkers/MongoChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Clusters;
+
+namespace Lykke.SettingsReader.Checkers
+{
+    internal class MongoChecker : ISettingsFieldChecker
+    {
+        private readonly bool _throwExceptionOnFail;
+
+        internal MongoChecker(bool throwExceptionOnFail)
+        {
+            _throwExceptionOnFail = throwExceptionOnFail;
+        }
+
+        public CheckFieldResult CheckField(object model, string propertyName, string value)
+        {
+            try
+            {
+                var mongoUrl = MongoUrl.Create(value);
+
+                var client = new MongoClient(mongoUrl);
+                
+                client.ListDatabases();
+                
+                return client.Cluster.Description.State == ClusterState.Connected
+                    ? CheckFieldResult.Ok(propertyName, value)
+                    : CheckFieldResult.Failed(propertyName, value, _throwExceptionOnFail);
+            }
+            catch (MongoConfigurationException)
+            {
+                return CheckFieldResult.Failed(propertyName, value, _throwExceptionOnFail);
+            }
+            catch (TimeoutException)
+            {
+                return CheckFieldResult.Failed(propertyName, value, _throwExceptionOnFail);
+            }
+        }
+    }
+}
