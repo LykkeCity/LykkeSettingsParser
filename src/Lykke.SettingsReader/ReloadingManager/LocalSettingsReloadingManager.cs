459a696703ac517cd6327887c376bd9c8a5a7e8f
Andrey Anokhin <yashmel@mail.ru>
2019-02-04 00:18:08 +0300

    Load settings with optional throw of an exception

--- src/Lykke.SettingsReader/ReloadingManager/LocalSettingsReloadingManager.cs
+++ src/Lykke.SettingsReader/ReloadingManager/LocalSettingsReloadingManager.cs
@@ -11,14 +11,18 @@ namespace Lykke.SettingsReader
     {
         private readonly string _path;
         private readonly Action<SlackNotificationOptions<TSettings>> _slackNotificationOptions;
+        private readonly bool _throwExceptionOnCheckError;
 
-        public LocalSettingsReloadingManager(string path, Action<SlackNotificationOptions<TSettings>> slackNotificationOptions)
+        public LocalSettingsReloadingManager(string path, 
+            Action<SlackNotificationOptions<TSettings>> slackNotificationOptions,
+            bool throwExceptionOnCheckError = false)
         {
             if (string.IsNullOrEmpty(path))
                 throw new ArgumentException("Path not specified.", nameof(path));
 
             _path = path;
             _slackNotificationOptions = slackNotificationOptions;
+            _throwExceptionOnCheckError = throwExceptionOnCheckError;
         }
 
         protected override async Task<TSettings> Load()
@@ -29,7 +33,21 @@ namespace Lykke.SettingsReader
                 var processingResult = await SettingsProcessor.ProcessForConfigurationAsync<TSettings>(content);
                 SetSettingsConfigurationRoot(processingResult.Item2);
 
-                Task.Run(() => SettingsProcessor.CheckDependenciesAsync(processingResult.Item1, _slackNotificationOptions));
+                if (!_throwExceptionOnCheckError)
+                {
+                    Task.Run(() => SettingsProcessor.CheckDependenciesAsync(processingResult.Item1, _slackNotificationOptions));
+                }
+                else
+                {
+                    var errorMessages = await SettingsProcessor.CheckDependenciesAsync(processingResult.Item1,
+                        _slackNotificationOptions);
+
+                    if (!string.IsNullOrEmpty(errorMessages))
+                    {
+                        throw new Exception($"Services check failed:{Environment.NewLine}{errorMessages} ");
+                    }
+                }
+                
                 
                 return processingResult.Item1;
             }
